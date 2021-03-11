6839720afa3a7d01826d883242ebab94dc9fcfcd
Orgonek <patryk.dragon@outlook.com>
2021-03-11 21:36:11 +0100

    Implemented file hashing function

--- hashing/src/hash_all.py
+++ hashing/src/hash_all.py
@@ -1,25 +1,49 @@
 import hashlib
+import timeit
 
 class HashAll:
-
+    """ Class used for perform text and file hashing """
     def __init__(self):
         pass
 
-    def perform_hashing(self, text : str):
+    def perform_hashing(self, text : str) -> dict:
+        """ Perfoms hashing using available functions and calculates the execution time for each algorithm """
         
         results = {}
 
         for algorithm in sorted(hashlib.algorithms_available):
             obj = hashlib.new(algorithm)
             obj.update(text.encode('utf-8'))
+
+            results[algorithm] = {}
             
+            start = timeit.default_timer()
+
             if 'shake' in algorithm:
-                results[algorithm] = obj.hexdigest(64)
+                results[algorithm]['hexdigest'] = obj.hexdigest(64)
             else:
-                results[algorithm] = obj.hexdigest()
+                results[algorithm]['hexdigest'] = obj.hexdigest()
+            
+            stop = timeit.default_timer()
+
+            results[algorithm]['time'] = (stop - start)
 
         return results
 
+    def perform_file_hashing(self, file_path : str, algorithm_name : str) -> str:
+        """ Performs file hashing using different algorithms """
+        
+        BUF_SIZE = 4096
+
+        algorithm_name = algorithm_name.lower() if algorithm_name.lower() in hashlib.algorithms_available else 'sha256'
+        algorithm = hashlib.new(algorithm_name)
+        
+        with open(file_path, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                algorithm.update(data)
 
 
-
+        return algorithm.hexdigest(64) if 'shake' in algorithm_name else algorithm.hexdigest()
--- hashing/src/main.py
+++ hashing/src/main.py
@@ -1,5 +1,10 @@
 from hash_all import HashAll
+import hashlib
+import os
 
 obj = HashAll()
 
-print(obj.perform_hashing("text"))
+path = os.path.join(os.path.expanduser('~'),'Downloads', 'ubuntu-20.04.2.0-desktop-amd64.iso')
+print(obj.perform_file_hashing(path, 'sha256'))
+#print(hashlib.algorithms_available)
+
