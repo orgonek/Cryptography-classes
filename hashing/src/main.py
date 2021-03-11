bc14c194df753e55201824dd52102710a492de30
Orgonek <patryk.dragon@outlook.com>
2021-03-11 23:21:19 +0100

    Added visualization of hashing time

--- hashing/src/main.py
+++ hashing/src/main.py
@@ -1,11 +1,14 @@
 from hash_all import HashAll
 import hashlib
 import os
+from utils import create_plot
 
 obj = HashAll()
 
 path = os.path.join(os.path.expanduser('~'),'Downloads', 'ubuntu-20.04.2.0-desktop-amd64.iso')
 
 print(obj.perform_file_hashing(path, 'sha256'))
+
+create_plot()
 
 
--- hashing/src/utils.py
+++ hashing/src/utils.py
@@ -1,1 +1,21 @@
+from string import ascii_letters
+import random
+import timeit
+from hash_all import HashAll
+import plotly.express as px
 
+
+def generate_data():
+    return [''.join(random.choices(ascii_letters, k = i**4)) for i in range(1, 21)]
+
+def create_plot():
+    data = generate_data()
+    results = []
+    h = HashAll()
+
+    for text in data:
+        results.append(timeit.timeit(lambda: h.perform_hashing(text), number=1))
+
+    fig = px.line(x=[len(i) for i in data], y=results)
+    fig.update_layout(title = 'Hashing time, depending on the amount of data', xaxis_title = 'Length of text', yaxis_title='Time in seconds')
+    fig.show()
