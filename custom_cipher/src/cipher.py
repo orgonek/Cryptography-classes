e9a11cd426fe6928779516ea3fe2db20b67a59e8
Orgonek <patryk.dragon@outlook.com>
2021-04-19 16:35:18 +0200

    Added some operations on rows

--- custom_cipher/src/cipher.py
+++ custom_cipher/src/cipher.py
@@ -4,6 +4,9 @@ import re
 
 
 class CustomCipher:
+    """
+    This class contains custom cipher implementation
+    """
 
     def __init__(self, key: int):
         self.key = key % 30
@@ -12,7 +15,12 @@ class CustomCipher:
         self.sub_alphabet = self._generate_sub_alphabet()
         self.rev_alphabet = self._generate_reversed_sub_alphabet()
 
-    def _generate_sub_alphabet(self):
+    def _generate_sub_alphabet(self) -> dict:
+        """
+        Generates substitution alphabet for each letter
+        Returns: dictionary with key value pairs
+
+        """
         sub_alphabet = {}
         for letter in self.alphabet:
             ix = ord(letter)
@@ -21,14 +29,58 @@ class CustomCipher:
 
         return sub_alphabet
 
-    def _generate_reversed_sub_alphabet(self):
+    def _generate_reversed_sub_alphabet(self) -> dict:
+        """
+        Generates reversed substitution alphabet for decrypting cipher
+        Returns: dictionary where key is number and value is letter
+
+        """
         return {
             value: letter
             for letter, values in self.sub_alphabet.items()
             for value in values
         }
 
-    def encrypt(self, filename):
+    @staticmethod
+    def _reverse_row(row: str):
+        """
+        Args:
+            row: line of cipher
+
+        Returns: reversed line of cipher
+
+        """
+        return row[::-1]
+
+    def _process_data(self, text: str):
+        """
+        Process data based on position in text
+
+        """
+        text_formatted = [text[i:i + 90] for i in range(0, len(text), 90)]
+
+        result = ''
+        for i, line in enumerate(text_formatted):
+            if i % 5 == 0:
+                half = len(line) // 2
+                result += self._reverse_row(line[:half])
+                result += line[half:]
+            elif i % 2 == 0 and i % 4 != 0:
+                result += self._reverse_row(line)
+            else:
+                result += line
+
+        return result
+
+    def encrypt(self, filename) -> str:
+        """
+        Encrypts file with custom cipher
+        Args:
+            filename: name of file to encrypt
+
+        Returns: encrypted text
+
+        """
         with open(filename) as file:
             data = file.read()
             data = re.sub("[^a-zA-Z]+", "", data)
@@ -41,26 +93,42 @@ class CustomCipher:
             else:
                 encrypted_text += letter
 
-        encrypted_text_format= [encrypted_text[i:i + 90] for i in range(0, len(encrypted_text), 90)]
-
-        with open(f'encrypted_{filename}', 'w+') as file:
-            for line in encrypted_text_format:
-                file.write(f'{line}\n')
+        encrypted_text = self._process_data(encrypted_text)
 
         return encrypted_text
 
     def decrypt(self, filename):
+        """
+        Decrypts custom cipher included in file
+        Args:
+            filename: name of file which should be decrypted
+
+        Returns: Decrypted text
+        """
         decrypted_text = ''
         with open(filename) as file:
             text = file.read()
             text = re.sub("[^0-9]+", "", text)
 
-        text_formatted = [text[i: i + 3] for i in range(0, len(text), 3)]
+        decrypted_text = self._process_data(text)
+
+        text_formatted = [decrypted_text[i: i + 3] for i in range(0, len(decrypted_text), 3)]
+        decrypted_text = ''
         for sign in text_formatted:
             decrypted_text += self.rev_alphabet[int(sign)]
 
-        decrypted_text_format = [decrypted_text[i:i + 90] for i in range(0, len(decrypted_text), 90)]
+        return decrypted_text
 
-        with open(f'decrypted_{filename}', 'w+') as file:
-            for line in decrypted_text_format:
+    @staticmethod
+    def save_to_file(text: str, filename: str):
+        """
+        Saves text to file, 90 characters per line
+        Args:
+            text: text to be saved
+            filename: name of file
+        """
+        text_formatted = [text[i:i + 90] for i in range(0, len(text), 90)]
+
+        with open(f'file_{filename}', 'w+') as file:
+            for line in text_formatted:
                 file.write(f'{line}\n')
--- custom_cipher/src/main.py
+++ custom_cipher/src/main.py
@@ -1,7 +1,9 @@
 from cipher import CustomCipher
 
-custom_cipher = CustomCipher(650)
-# custom_cipher.encrypt('sample.txt')
-custom_cipher.decrypt('encrypted_sample.txt')
+custom_cipher = CustomCipher(2)
+encrypted_text = custom_cipher.encrypt('sample.txt')
+custom_cipher.save_to_file(encrypted_text, 'encrypted.txt')
+decrypted_file = custom_cipher.decrypt('file_encrypted.txt')
+custom_cipher = custom_cipher.save_to_file(decrypted_file, 'Solved.txt')
 
 
